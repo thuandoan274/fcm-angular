9a0834103eed35ad4e4095b8676d95f6adbc5449
thuandoan274 <thuandoan@novahub.vn>
2018-07-13 15:29:12 +0700

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCsvUV79uQX3ZEK6HaA_Yg0nKfjIMTEIj0",
+    authDomain: "my-fcm-test-23814.firebaseapp.com",
+    databaseURL: "https://my-fcm-test-23814.firebaseio.com",
+    projectId: "my-fcm-test-23814",
+    storageBucket: "my-fcm-test-23814.appspot.com",
+    messagingSenderId: "52868937389"
+  }
 };
